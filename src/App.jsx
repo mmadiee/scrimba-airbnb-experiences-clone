d9285546b77a9a63d798104b891e82ec7891367d
mmadiee <149312891+mmadiee@users.noreply.github.com>
2024-08-08 15:32:24 +0800

    Added Hero Component with stylings

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css"
 import Navbar from "./components/navbar"
+import Hero from "./components/hero"
 
 export default function App() {
   return (
       <>
         <Navbar />
+        <Hero />
       </>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import photo from "../images/photo-grid.png";
+
+export default function Hero() {
+  return (
+    <section className="hero">
+      <div className="text-area">
+        <h1>Online Experiences</h1>
+        <p>
+          Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+        </p>
+      </div>
+      <img src={photo} alt="photo grid" className="photo-grid" />
+    </section>
+  );
+}
