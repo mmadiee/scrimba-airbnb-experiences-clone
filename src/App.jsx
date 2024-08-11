c966e549978e8b2ba00f93679d4071c0e8aeed3a
mmadiee <149312891+mmadiee@users.noreply.github.com>
2024-08-11 16:31:35 +0800

    Dynamically displayed cards using .map() function

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,27 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
+import cardsArray from "./components/data";
 
 export default function App() {
+  const cardElements = cardsArray.map((card) => {
+    return (
+      <Card
+        key={card.id}
+        coverImg={card.coverImg}
+        rating={card.stats.rating}
+        reviewCount={card.stats.reviewCount}
+        location={card.location}
+        price={card.price}
+        title={card.title}
+      />
+    );
+  });
   return (
     <>
       <Navbar />
       <Hero />
-      <Card
-        img="katie-zaferes.png"
-        rating="5.0"
-        reviewCount={6}
-        country="USA"
-        title="Life Lessons with Katie Zaferes"
-        price={136}
-      />
+      <section className="cards">{cardElements}</section>
     </>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,20 +3,23 @@ import star from "/images/star.png";
 
 export default function Card(prop) {
   return (
-    <section className="cards">
-      <div className="card">
-        <div className="image-area">
-          <p className="status">SOLD OUT</p>
-          <img className="card-img" src={`/images/${prop.img}`}/>
-        </div>  
-        <span>
-          <img className="star" src={star} alt="Star Icon"/>
-          <p>{prop.rating}</p>
-          <p className="rating-gray"> ({prop.reviewCount}) &bull; {prop.country} </p>
-        </span>
-        <h2>{prop.title}</h2>
-        <p><strong>From {prop.price} </strong>/ person</p>
+    <div className="card">
+      <div className="image-area">
+        <p className="status">SOLD OUT</p>
+        <img className="card-img" src={`/images/${prop.coverImg}`} />
       </div>
-    </section>
+      <span>
+        <img className="star" src={star} alt="Star Icon" />
+        <p>{prop.rating}</p>
+        <p className="rating-gray">
+          {" "}
+          ({prop.reviewCount}) &bull; {prop.location}{" "}
+        </p>
+      </span>
+      <h2>{prop.title}</h2>
+      <p>
+        <strong>From {prop.price} </strong>/ person
+      </p>
+    </div>
   );
 }
